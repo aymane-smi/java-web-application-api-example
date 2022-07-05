918bb20ee73d08a967e509ef36d852b567d3637a
aymane <38135532+aymane-smi@users.noreply.github.com>
2022-07-05 17:34:23 +0100

    Create constants.java

---
+++ auth-system-spring/src/main/java/com/example/authsystemspring/Constants/constants.java
@@ -0,0 +1,7 @@
+package com.example.authsystemspring;
+
+public class constants {
+	public static final String KEY = "API_KEY";
+	
+	public static final int DURATION = 2 * 60 * 60 * 1000;//setting 2h in ms for token life time 
+}
